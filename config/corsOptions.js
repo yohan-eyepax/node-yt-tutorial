f61aebb397e6aa2f0d28bc076500e0faff773c57
Yohan Alahakoon <yohanalahakoon@EYEPAXMM-07.local>
2022-08-24 14:33:00 +0530

    initial code

---
+++ config/corsOptions.js
@@ -0,0 +1,18 @@
+const whitelist = [
+    'https://www.yousite.com', 
+    'http://127.0.0.1:5500', 
+    'http://localhost:3500'
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin){
+            callback(null, true);
+        }else{
+            callback(new Error('Not allowed by cors'));
+        }
+    },
+    optionsSuccessStauts: 200
+}
+
+module.exports = corsOptions;
